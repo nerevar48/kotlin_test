03ea3287d5fddb2305f115701b8aa53613ae7666
m.livanov <m.livanov@turnkeye.com>
2020-07-15 16:56:54 +0400

    tree variant

--- src/IP-Addr-Counter.kt
+++ src/IP-Addr-Counter.kt
@@ -7,10 +7,10 @@ import kotlin.system.measureTimeMillis
 
 suspend fun main(args: Array<String>) {
     var remain = ""
-    val bufferSize = 1024*1024*8
+    val bufferSize = 1024*1024*16
     var length: Int
     var linesCounter: Long = 0
-    val trie = Trie<Byte>()
+    val trie = Trie<String>()
 
     val executionTime = measureTimeMillis {
         // lines 8 000 000 000
@@ -25,41 +25,54 @@ suspend fun main(args: Array<String>) {
                     break
 
                 val bufferString = buffer.toString(Charset.defaultCharset())
+
                 // деление на строки
-                val list = bufferString.split("\n")
-
-                linesCounter += list.size
-                /**
-                 *  запоминание последнего элемента, т.к он обрезан при доставании из файла
-                 */
-                val last = list.lastIndex
-
-                for (i in 0 until last) {
-                    if (list[i] == "" && remain == "") {
-                        continue
-                    }
-                    var line = list[i]
-                    // приклеиваем остаток из прошлой итерации к первой строке в новой итерации
+                var currentOffset = 0
+                var nextIndex = bufferString.indexOf('\n', currentOffset, true)
+                val arrayToTrie = arrayOf<Byte>(0, 0, 0, 0)
+                do {
+                    var line = bufferString.substring(currentOffset, nextIndex)
                     if (remain != "") {
                         line = remain + line
                         remain = ""
                     }
-                    // добавляем в дерево
-                    trie.add(line)
-                }
+                    var currentOffset1 = 0
+                    var nextIndex1 = line.indexOf('.', currentOffset1, true)
+                    var index = 0
+                    do {
+                        val str = line.substring(currentOffset1, nextIndex1)
+                        val int = str.toInt()
+                        val byte = int.toByte()
 
-                remain = list[last]
+                        arrayToTrie[index] = byte
+                        index++
+
+                        currentOffset1 = nextIndex1 + 1
+                        nextIndex1 = line.indexOf('.', currentOffset1, true)
+
+                    } while (nextIndex1 != -1)
+                    val str = line.substring(currentOffset1, line.length)
+                    val int = str.toInt()
+                    val byte = int.toByte()
+                    arrayToTrie[index] = byte
+                    trie.add(arrayToTrie)
+
+                    currentOffset = nextIndex + 1
+                    nextIndex = bufferString.indexOf('\n', currentOffset, true)
+                    linesCounter ++
+                } while (nextIndex != -1)
+
+                remain = bufferString.substring(currentOffset, bufferString.length)
 
                 println("lines remain - ${8000000000-linesCounter}")
 //                println("unique ip - ${trie.size()}")
-
-                System.gc()
+//                System.gc()
             }
             // последний остаток из файла
-            remain = """[^\d\.]""".toRegex().replace(remain.trim(), "")
-            if (remain != "") {
-                trie.add(remain, true)
-            }
+//            remain = """[^\d\.]""".toRegex().replace(remain.trim(), "")
+//            if (remain != "") {
+//                trie.add(remain, true)
+//            }
         }
 
         reader.close()
@@ -69,25 +82,15 @@ suspend fun main(args: Array<String>) {
     println("${executionTime/1000} seconds of execute")
 }
 
+// объединено в 1 фйайл для удобства чтения
+
 data class Node<T>(val ch: HashMap<Byte, Node<T>?> = hashMapOf())
 
-class Trie<T>(val root: Node<T> =  Node<T>()) {
+class Trie<T>(private val root: Node<T> =  Node()) {
 
-    fun add(values: String, clean: Boolean = false) {
-        val varsString: Sequence<String>
-        val vars: Sequence<Byte>
-
-        // превращаем строку в массив байтов
-        if (!clean) {
-            varsString = values.split('.').asSequence()
-            vars = varsString.map {
-                it.toShort().toByte()
-            }
-        } else {
-            varsString = values.split('.').asSequence()
-            vars = varsString.map {"""[^\d]""".toRegex().replace(it.trim(), "").toShort().toByte() }
-        }
-
+    fun add(values: Array<Byte>, clean: Boolean = false) {
+        val array = arrayOf(values[0], values[1], values[2], values[3])
+        val childrenLinks = arrayOf<HashMap<Byte, Node<T>?>?>(root.ch, null, null, null)
         /**
          *  формируем дерево байтов, например:
          *               90
@@ -98,19 +101,16 @@ class Trie<T>(val root: Node<T> =  Node<T>()) {
          *          /      /\    \
          *         1     83 77   56
          */
-        var children = root.ch
-        vars.forEachIndexed { i, value ->
-            if (children[value] == null) {
+        array.forEachIndexed { i, value ->
+            if (childrenLinks[i]!![value] == null) {
                 if (i == 3) {
-                    children[value] = null
+                    childrenLinks[i]!![value] = null
                 } else {
-                    val node: Node<T> = Node()
-                    children[value] = node
-                    children = node.ch
+                    childrenLinks[i]!![value] = Node()
+                    childrenLinks[i+1] = childrenLinks[i]!![value]!!.ch
                 }
             } else {
-                val node: Node<T> = children[value]!!
-                children = node.ch
+                childrenLinks[i+1] = childrenLinks[i]!![value]!!.ch
             }
         }
     }
